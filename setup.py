691ced1087212c4d9960281a95daa15ff3505c00
Raymond Butcher <ray.butcher@claranet.uk>
2018-03-20 17:33:52 +0000

    Fix dateutil requirement

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='libfaketimefs-ctl',
-    version='0.0.1',
+    version='0.0.2',
     description='libfaketimefs controller',
     author='Raymond Butcher',
     author_email='ray.butcher@claranet.uk',
@@ -18,6 +18,6 @@ setup(
     ),
     install_requires=(
         'boto3',
-        'dateutil',
+        'py-dateutil',
     ),
 )
